0f9321a3a4a58798dd11a3ad67e5509c7fc404d1
Vish-Linux <127185682+Vishy70@users.noreply.github.com>
2024-07-19 19:46:29 +0530

    Controlled access to admin (update middleware)

--- library-r-s-n/supabase/middleware.js
+++ library-r-s-n/supabase/middleware.js
@@ -56,6 +56,25 @@ export async function updateSession(request) {
     return NextResponse.redirect(url);
   }
 
+  let { data } = await supabase
+  .from('customers')
+  .select('*')
+  .eq('customer_id', user.id);
+  [data] = data
+
+  if (
+    !data.is_admin &&
+    request.nextUrl.pathname.startsWith("/admin")
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    console.log(request.nextUrl.pathname)
+    console.log("Entered if!")
+    const url = request.nextUrl.clone();
+    url.pathname = "/home";
+    return NextResponse.redirect(url);
+  }
+
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
